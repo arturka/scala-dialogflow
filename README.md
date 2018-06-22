87f7fabbd150e35d2d227cf20a656d09b0257e59
AbdalimovArtur <a.abdalimov.97@gmail.com>
2018-06-22 14:34:25 +0600

    Add README.md Update project id derived from environment

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dialogflow v2 client under Scala
+
+### set environment variable PROJECT_ID 
+- You can find it in your agent settings
+
+### set environment variable GOOGLE_APPLICATION_CREDENTIALS
+- More about [Cloud API authentication](https://cloud.google.com/docs/authentication/getting-started)
--- src/main/scala/BootApp.scala
+++ src/main/scala/BootApp.scala
@@ -5,11 +5,13 @@ import com.google.cloud.dialogflow.v2._
 object BootApp extends App {
 
   val sessionId = UUID.randomUUID().toString
-  val projectId = ""
+  val projectId = System.getenv("PROJECT_ID")
+
+  println(s"ProjectId = $projectId")
 
   val sessionClient: SessionsClient = SessionsClient.create()
   val session = SessionName.of(projectId, sessionId)
-  val textInput = TextInput.newBuilder().setText("Привет").setLanguageCode("ru-RU")
+  val textInput = TextInput.newBuilder().setText("Hi").setLanguageCode("en-EN")
   val queryInput = QueryInput.newBuilder().setText(textInput).build()
 
   println(sessionClient.detectIntent(session, queryInput).getQueryResult.getFulfillmentMessagesList.get(0))
